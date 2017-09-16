9469902e909d6a7a68ced8dbb3bbf2d7ef06766e
bowei <bowei.han100@gmail.com>
2017-09-16 17:43:26 -0400

    initial spring boot infrastructure

---
+++ README.md
@@ -0,0 +1,2 @@
+Wrapper: gradle wrapper --gradle-version 2.13
+Build: ./gradlew build && java -jar build/libs/gs-spring-boot-0.1.0.jar
---
+++ src/main/java/actuator/HealthCheck.java
@@ -0,0 +1,20 @@
+package com.dineshonjava.sba;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+
+public class HealthCheck implements HealthIndicator{
+
+    @Override
+    public Health health() {
+        int errorCode = check();
+        if (errorCode != 0) {
+            return Health.down().withDetail("Error Code", errorCode).build();
+        }
+        return Health.up().build();
+    }
+
+    private int check() {
+        return 0;
+    }
+}
