5b2267f1900f97da20ad971bcd6e9bd74b0eda05
bowei <bowei.han100@gmail.com>
2017-09-16 22:01:03 -0400

    remove unneeded class

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -19,7 +19,11 @@ public class Application {
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
 
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
+            System.out.println(
+                    "***************************************************** \n" +
+                            "HERE ARE THE BEANS \n" +
+                            "*****************************************************"
+            );
 
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
